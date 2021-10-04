3375634463b1a8ef46d758ce0933bf371feb8453
Ronald Magno <dlanoronald123@gmail.com>
2021-10-04 23:00:54 +0800

    Finished the funtion part

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,47 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-
+import {map} from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
-export class CartService {
+export class ApiService {
 
-  public cartItemList : any =[]
-  public productList = new BehaviorSubject<any>([]);
-  public search = new BehaviorSubject<string>("");
+  constructor(private http : HttpClient) { }
 
-  constructor() { }
-  getProducts(){
-    return this.productList.asObservable();
-  }
-
-  setProduct(product : any){
-    this.cartItemList.push(...product);
-    this.productList.next(product);
-  }
-  addtoCart(product : any){
-    this.cartItemList.push(product);
-    this.productList.next(this.cartItemList);
-    this.getTotalPrice();
-    console.log(this.cartItemList)
-  }
-  getTotalPrice() : number{
-    let grandTotal = 0;
-    this.cartItemList.map((a:any)=>{
-      grandTotal += a.total;
-    })
-    return grandTotal;
-  }
-  removeCartItem(product: any){
-    this.cartItemList.map((a:any, index:any)=>{
-      if(product.id=== a.id){
-        this.cartItemList.splice(index,1);
-      }
-    })
-    this.productList.next(this.cartItemList);
-  }
-  removeAllCart(){
-    this.cartItemList = []
-    this.productList.next(this.cartItemList);
+  getProduct(){
+    return this.http.get<any>("https://fakestoreapi.com/products")
+    .pipe(map((res:any)=>{
+      return res;
+    }))
   }
 }
